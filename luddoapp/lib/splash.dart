a68999b9abcb0d04f012bc9b895d7f367a06b25b
AbdulRehmanTariq011 <abdulrehman0737266@gmail.com>
2021-10-20 01:43:39 +0500

    Splash Screen
    adding splash screen with time (5) seconds. After splash screen main screen will display.

--- luddoapp/lib/splash.dart
+++ luddoapp/lib/splash.dart
@@ -1,1 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:luddoapp/screen2.dart';
 
+class splash extends StatefulWidget {
+  const splash({Key? key}) : super(key: key);
+
+  @override
+  _splashState createState() => _splashState();
+}
+
+class _splashState extends State<splash> {
+  @override
+  void initState() {
+    super.initState();
+    _navigatetohome();
+  }
+
+  _navigatetohome() async {
+    await Future.delayed(Duration(milliseconds: 5000), () {});
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => MyApp()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey,
+        body: Stack(fit: StackFit.expand, children: [
+          new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(bottom: 50.0),
+                child: new Image.asset(
+                  'images/ludo.png',
+                  height: 200.0,
+                  fit: BoxFit.scaleDown,
+                ),
+              ),
+              Text(
+                "Welcom to Luddo Game",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.red,
+                ),
+              )
+            ],
+          ),
+        ]));
+  }
+}
