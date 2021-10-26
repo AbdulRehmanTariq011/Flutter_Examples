28b5b7d3f0834e907ac9344d420b6e339b1e4b11
AbdulRehmanTariq011 <abdulrehman0737266@gmail.com>
2021-10-26 16:13:26 +0500

    Adding splash screen

---
+++ xylophone/lib/splash.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:xylophone/screen2.dart';
+
+import 'main.dart';
+
+class splash extends StatefulWidget {
+  const splash({Key? key}) : super(key: key);
+
+  @override
+  _splashState createState() => _splashState();
+}
+
+class _splashState extends State<splash> {
+  @override
+  void initState() {
+    super.initState();
+    _navigatetohome();
+  }
+
+  _navigatetohome() async {
+    await Future.delayed(Duration(milliseconds: 3000), () {});
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => XylophoneApp()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey,
+        body: Stack(fit: StackFit.expand, children: [
+          new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(bottom: 50.0),
+                child: new Image.asset(
+                  'images/xp.jpg',
+                  height: 150.0,
+                  fit: BoxFit.scaleDown,
+                ),
+              ),
+              Text(
+                "Welcom to Luddo Game",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.red,
+                ),
+              )
+            ],
+          ),
+        ]));
+  }
+}
